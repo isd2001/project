defc9f9fe21d860f31cfc981013e385fa15585b5
fafragargzxcvz <dlaalstjd123@gmail.com>
2018-10-29 12:39:33 +0900

    2018 10 29 12:39 IMS

--- src/app/controller/Nboard/NboardController.java
+++ src/app/controller/Nboard/NboardController.java
@@ -1,18 +1,47 @@
 package app.controller.Nboard;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+
+import app.models.NboardRepository;
 
 @Controller
 @RequestMapping("/Nboard")
 public class NboardController {
-
+	@Autowired
+	NboardRepository nboard;
+	
+	
+	// 공지사항 이동
 	@GetMapping("/list.do")
-	public String NboardHandler() {
+	public String NboardHandler(WebRequest wreq,@RequestParam Map map,Model model) {
+		
+		List<Map> Allboard =nboard.getAllNboard();
+		System.out.println("allboard > "+ Allboard);
+		
+		
+		// webrequest - > 보낼때
+		// @requestparam -> 같고올때
+		// model -> 보낼때 , 일회성
+		
+		wreq.setAttribute("list", Allboard, WebRequest.SCOPE_REQUEST);
+		
 		
 		return "Nboard.list";
-	}
+	}//end NboardHandle
 	
-}
+	
+	
+	
+	
+	
+}//end class
---
+++ src/app/models/NboardRepository.java
@@ -0,0 +1,23 @@
+package app.models;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NboardRepository {
+
+	@Autowired
+	SqlSessionTemplate template;
+	
+	// 전체 공지사항 출력
+	public List<Map> getAllNboard(){
+		return template.selectList("member_board.getAllNboard");
+	}//end getAllNboard
+	
+	
+	
+}//end class
